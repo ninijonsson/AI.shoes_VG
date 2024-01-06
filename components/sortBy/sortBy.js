72599d3055c4ad41ecc2d522fec35e43fe533f41
ninijonsson <143434132+ninijonsson@users.noreply.github.com>
2024-01-06 18:37:51 +0100

    Sort by price (low to high, high to low)

--- components/sortBy/sortBy.js
+++ components/sortBy/sortBy.js
@@ -1,13 +1,40 @@
 "use strict";
 
-function renderSortBy(parent) {
-    const container = document.createElement("div");
-    container.classList.add("sortByContainer");
-    parent.appendChild(container);
+document.addEventListener('DOMContentLoaded', function () {
+    // Your code here
 
-    container.innerHTML = `
-    <select id="sort_by">
-        <option value="lowestValue">PRICE: LOW TO HIGH PRICE</option>
-    </select>
-    `;
-}
+    const sortSelect = document.getElementById('sort_by');
+
+    console.log(sortSelect);
+
+    // Add change event listener to the sort select
+    sortSelect.addEventListener('change', sortProducts);
+
+    function sortProducts() {
+        const sortOption = sortSelect.value;
+
+        console.log(sortSelect.value);
+
+        let sortedProducts;
+
+        switch (sortOption) {
+            case 'lowestPrice':
+                sortedProducts = SHOES.slice().sort((a, b) => a.price - b.price);
+                break;
+            case 'highestPrice':
+                sortedProducts = SHOES.slice().sort((a, b) => b.price - a.price);
+                break;
+            case "alphabeticalOrder":
+                sortedProducts = COUNTRIES.name.sort();
+                console.log(sortedProducts);
+                break;
+            default:
+                // Default sorting (no sorting)
+                sortedProducts = SHOES.slice();
+        }
+
+
+        renderShoeList(structureContainers.bottom, sortedProducts);
+    }
+
+});
